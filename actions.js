612284e781ab23aa9cf56e0449dc3decbe4587e1
Sp1gola <moro.matteo007@gmail.com>
2026-06-17 09:35:40 +0200

    added movies section

--- actions.js
+++ actions.js
@@ -4,6 +4,7 @@ const desc = document.querySelector(".desc");
 const prg = document.querySelector(".prg");
 const sport = document.querySelector(".sport");
 const music = document.querySelector(".music");
+const tv = document.querySelector(".tv");
 const love = document.querySelector(".love");
 
 window.addEventListener("scroll", () => {
@@ -18,37 +19,47 @@ window.addEventListener("scroll", () => {
         prg.classList.remove("show");
         sport.classList.remove("show");
         music.classList.remove("show");
+        tv.classList.remove("show");
         love.classList.remove("show");
     }
-    else if (pct > 1 && pct <= 30) {
+    else if (pct > 1 && pct <= 25) {
         welcome.classList.add("hidden");
         name.classList.add("hidden");
         desc.classList.add("show");
         prg.classList.remove("show");
         sport.classList.remove("show");
         music.classList.remove("show");
+        tv.classList.remove("show");
         love.classList.remove("show");
     }
-    else if (pct > 30 && pct <= 55) {
+    else if (pct > 25 && pct <= 45) {
         desc.classList.remove("show");
         prg.classList.add("show");
         sport.classList.remove("show");
         music.classList.remove("show");
+        tv.classList.remove("show");
         love.classList.remove("show");
     }
-    else if (pct > 55 && pct <= 78) {
+    else if (pct > 45 && pct <= 62) {
         prg.classList.remove("show");
         sport.classList.add("show");
         music.classList.remove("show");
+        tv.classList.remove("show");
         love.classList.remove("show");
     }
-    else if (pct > 78 && pct <= 95) {
+    else if (pct > 62 && pct <= 78) {
         sport.classList.remove("show");
         music.classList.add("show");
+        tv.classList.remove("show");
         love.classList.remove("show");
     }
-    else if (pct > 95) {
+    else if (pct > 78 && pct <= 92) {
         music.classList.remove("show");
+        tv.classList.add("show");
+        love.classList.remove("show");
+    }
+    else if (pct > 92) {
+        tv.classList.remove("show");
         love.classList.add("show");
     }
 });
