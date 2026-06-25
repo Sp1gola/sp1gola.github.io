a4b7e07db4950e8ac050f850c97525df0efd8f5a
Sp1gola <moro.matteo007@gmail.com>
2026-06-25 09:34:35 +0200

    added future section, end, music fix

--- actions.js
+++ actions.js
@@ -5,13 +5,15 @@ const prg = document.querySelector(".prg");
 const sport = document.querySelector(".sport");
 const music = document.querySelector(".music");
 const tv = document.querySelector(".tv");
+const future = document.querySelector(".future");
 const love = document.querySelector(".love");
-
+const end = document.querySelector(".end");
+ 
 window.addEventListener("scroll", () => {
     const scrollY = window.scrollY;
     const maxScroll = document.documentElement.scrollHeight - window.innerHeight;
     const pct = (scrollY / maxScroll) * 100;
-
+ 
     if (pct <= 1) {
         welcome.classList.remove("hidden");
         name.classList.remove("hidden");
@@ -20,9 +22,11 @@ window.addEventListener("scroll", () => {
         sport.classList.remove("show");
         music.classList.remove("show");
         tv.classList.remove("show");
+        future.classList.remove("show");
         love.classList.remove("show");
+        end.classList.remove("show");
     }
-    else if (pct > 1 && pct <= 25) {
+    else if (pct > 1 && pct <= 13) {
         welcome.classList.add("hidden");
         name.classList.add("hidden");
         desc.classList.add("show");
@@ -30,37 +34,58 @@ window.addEventListener("scroll", () => {
         sport.classList.remove("show");
         music.classList.remove("show");
         tv.classList.remove("show");
+        future.classList.remove("show");
         love.classList.remove("show");
+        end.classList.remove("show");
     }
-    else if (pct > 25 && pct <= 45) {
+    else if (pct > 13 && pct <= 26) {
         desc.classList.remove("show");
         prg.classList.add("show");
         sport.classList.remove("show");
         music.classList.remove("show");
         tv.classList.remove("show");
+        future.classList.remove("show");
         love.classList.remove("show");
+        end.classList.remove("show");
     }
-    else if (pct > 45 && pct <= 62) {
+    else if (pct > 26 && pct <= 38) {
         prg.classList.remove("show");
         sport.classList.add("show");
         music.classList.remove("show");
         tv.classList.remove("show");
+        future.classList.remove("show");
         love.classList.remove("show");
+        end.classList.remove("show");
     }
-    else if (pct > 62 && pct <= 78) {
+    else if (pct > 38 && pct <= 51) {
         sport.classList.remove("show");
         music.classList.add("show");
         tv.classList.remove("show");
+        future.classList.remove("show");
         love.classList.remove("show");
+        end.classList.remove("show");
     }
-    else if (pct > 78 && pct <= 92) {
+    else if (pct > 51 && pct <= 63) {
         music.classList.remove("show");
         tv.classList.add("show");
+        future.classList.remove("show");
         love.classList.remove("show");
+        end.classList.remove("show");
     }
-    else if (pct > 92) {
+    else if (pct > 63 && pct <= 75) {
         tv.classList.remove("show");
+        future.classList.add("show");
+        love.classList.remove("show");
+        end.classList.remove("show");
+    }
+    else if (pct > 75 && pct <= 88) {
+        future.classList.remove("show");
         love.classList.add("show");
+        end.classList.remove("show");
+    }
+    else if (pct > 88) {
+        love.classList.remove("show");
+        end.classList.add("show");
     }
 });
 
